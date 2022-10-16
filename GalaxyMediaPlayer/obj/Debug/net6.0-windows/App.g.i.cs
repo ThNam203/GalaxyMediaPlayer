f8b616c5bc55bf3b93811a8ffa761485ca1bef7e
ptdat4823 <phamtiendat4823@gmail.com>
2022-10-16 17:05:42 +0700

    custom navbar button (not working because of bugs)

--- GalaxyMediaPlayer/obj/Debug/net6.0-windows/App.g.i.cs
+++ GalaxyMediaPlayer/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace GalaxyMediaPlayer {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace GalaxyMediaPlayer {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public static void Main() {
             GalaxyMediaPlayer.App app = new GalaxyMediaPlayer.App();
             app.InitializeComponent();
