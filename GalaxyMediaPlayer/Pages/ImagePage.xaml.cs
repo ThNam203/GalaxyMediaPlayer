a477b4b90d565ea23150e5e8b7cbc8255df0fcd0
ptdat4823 <phamtiendat4823@gmail.com>
2022-11-19 20:33:34 +0700

    done add ImagePage

---
+++ GalaxyMediaPlayer/Pages/ImagePage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+
+
+namespace GalaxyMediaPlayer.Pages
+{
+    /// <summary>
+    /// Interaction logic for ImagePage.xaml
+    /// </summary>
+    public partial class ImagePage : Page
+    {
+        public ImagePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Open Image";
+            dialog.Filter = "jpg files (*.jpg)|*.jpg|png files (*.png)|*.png|jpeg files (*.jpeg)|*.jpeg";
+            dialog.Multiselect = true;
+            if (dialog.ShowDialog() == true)
+            {
+                
+            }
+        }
+    }
+}
