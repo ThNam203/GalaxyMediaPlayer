82c505eaac34997e836918ab4107885fbbfbc1d6
Nam Huỳnh <21522356@ms.uit.edu.vn>
2022-12-28 21:28:39 +0700

    refactor playlist page

--- GalaxyMediaPlayer/Pages/NavContentPages/PlaylistPage.xaml.cs
+++ GalaxyMediaPlayer/Pages/NavContentPages/PlaylistPage.xaml.cs
@@ -2,200 +2,179 @@
 using GalaxyMediaPlayer.Helpers;
 using GalaxyMediaPlayer.Models;
 using GalaxyMediaPlayer.UserControls;
-using GalaxyMediaPlayer.UserControls.PlaylistControls;
 using Microsoft.Win32;
 using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Data;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
 
 namespace GalaxyMediaPlayer.Pages.NavContentPages
 {
-    /// <summary>
-    /// Interaction logic for PlaylistPage.xaml
-    /// </summary>
     public partial class PlaylistPage : Page
     {
-        private ObservableCollection<SongPlaylistModel> playlists;
-        private ObservableCollection<SongInfor> currentChosenPlaylistSongs = new ObservableCollection<SongInfor>();
-        private List<int> deleteIndices = new List<int>();
+        public static Button NewPlaylistBtn;
+        public static Button BackBtn;
+        public static Button AddNewSongToPlaylistBtn;
+        public static TextBlock PlaylistNameHeader;
+        public static ComboBox CbSortPlaylistBy;
+        public static StackPanel ChooseCategoryPanel;
+
+        public static Action<object, RoutedEventArgs> NewPlaylistBtn_Click;
+
+        public enum PlaylistPageType
+        {
+            Music,
+            Image,
+            Video,
+        }
+
+        PlaylistPageType currentPlaylistType = PlaylistPageType.Music;
+
         public PlaylistPage()
         {
             InitializeComponent();
 
-            playlists = new ObservableCollection<SongPlaylistModel>(PlaylistDatabaseAccess.LoadPlaylists());
+            NewPlaylistBtn = newPlaylistBtn;
+            PlaylistNameHeader = playlistNameHeader;
+            AddNewSongToPlaylistBtn = addNewSongToPlaylistBtn;
+            CbSortPlaylistBy = cbSortPlaylistBy;
+            BackBtn = backBtn;
+            ChooseCategoryPanel = chooseCategoryPanel;
+            NewPlaylistBtn_Click = this.newPlaylistBtn_Click;
 
-            playlistListBox.ItemsSource = playlists;
-            playlistSongsDataGrid.ItemsSource = currentChosenPlaylistSongs;
-
-            if (playlists.Count <= 0)
-            {
-                emptyPlaylistBorder.Visibility = Visibility.Visible;
-                newPlaylistBtn.Visibility = Visibility.Collapsed;
-            }
+            PageFrame.Navigate(new Uri("/Pages/PlaylistPagePages/MusicPlaylistPage.xaml", UriKind.Relative));
         }
 
-        private void listBoxItem_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void showMusicPlaylistsBtn_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            SongPlaylistModel? playlist;
-            playlist = playlistListBox.SelectedItem as SongPlaylistModel;
+            showMusicPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.White;
+            showVideosPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.Transparent;
+            showImagesPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.Transparent;
 
-            if (playlist != null && e.ClickCount == 2)
-            {
-                MainPage.currentMusicBrowsingFolder = "Playlist/" + playlist.Name;
-
-                currentChosenPlaylistSongs.Clear();
-                foreach (SongInfor songInfor in PlaylistSongsDatabaseAccess.LoadSongsFromPlaylistId(playlist.Id))
-                {
-                    if (File.Exists(songInfor.Path))
-                        currentChosenPlaylistSongs.Add(songInfor);
-                    else PlaylistSongsDatabaseAccess.DeleteSong(songInfor);
-                }
-
-                MainPage.Instance.ChangeButtonsViewOnOpenFolder(forceShow: false);
-                MainPage.Instance.ChangeAdditionControlVisibilityInInforGrid(false);
-
-                playlistNameHeader.Text = playlist.Name;
-                playlistListBox.Visibility = Visibility.Collapsed;
-                cbSortPlaylistBy.Visibility = Visibility.Collapsed;
-                playlistSongsDataGrid.Visibility = Visibility.Visible;
-                BackBtn.Visibility = Visibility.Visible;
-                addNewSongToPlaylistBtn.Visibility = Visibility.Visible;
-                newPlaylistBtn.Visibility = Visibility.Collapsed;
-            }
+            currentPlaylistType = PlaylistPageType.Music;
+            PageFrame.Navigate(new Uri("/Pages/PlaylistPagePages/MusicPlaylistPage.xaml", UriKind.Relative));
         }
 
-        private void listBoxItem_MouseRightButtonDown(
-            object sender, 
-            MouseButtonEventArgs e)
+        private void showVideosPlaylistsBtn_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            SongPlaylistModel? playlist;
-            playlist = playlistListBox.SelectedItem as SongPlaylistModel;
+            showVideosPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.White;
+            showMusicPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.Transparent;
+            showImagesPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.Transparent;
 
-            if (playlist != null)
-            {
-                PlaylistRightClickDialog dialog = new PlaylistRightClickDialog(
-                    onRenameButtonClick: RenamePlaylist, 
-                    onDeleteButtonClick: RemovePlaylist,
-                    currentName: playlist.Name);
-
-                int left = Convert.ToInt32(e.GetPosition(MainWindow.Instance as IInputElement).X);
-                int top = Convert.ToInt32(e.GetPosition(MainWindow.Instance as IInputElement).Y);
-                MainWindow.ShowCustomMessageBox(dialog, left: left, top: top);
-                e.Handled = true;
-            }
+            currentPlaylistType = PlaylistPageType.Video;
+            PageFrame.Navigate(new Uri("/Pages/PlaylistPagePages/Page1.xaml", UriKind.Relative));
         }
 
-        // DataGridRow hold songs that is currently in the chosen playlist
-        private void DataGridRow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        private void showImagesPlaylistsBtn_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            SongInfor? chosenSong;
-            chosenSong = playlistSongsDataGrid.SelectedItem as SongInfor;
+            showImagesPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.White;
+            showMusicPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.Transparent;
+            showVideosPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.Transparent;
 
-            // Nam: indicates that a song is chosen (not outside)
-            if (chosenSong != null)
-            {
-                List<string> songs = playlistSongsDataGrid.Items.Cast<SongInfor>().Select(s => s.Path).ToList();
-                MyMediaPlayer.SetNewPlaylist(songs);
-                MyMediaPlayer.SetPositionInPlaylist(playlistSongsDataGrid.SelectedIndex);
-                MyMediaPlayer.PlayCurrentSong();
-                e.Handled = true;
-            }
+            currentPlaylistType = PlaylistPageType.Image;
+            PageFrame.Navigate(new Uri("/Pages/PlaylistPagePages/Page1.xaml", UriKind.Relative));
         }
 
         // Show rename, delete table
         private void newPlaylistBtn_Click(object sender, RoutedEventArgs e)
         {
-            UserControls.NewPlaylistControl newPlaylistControl = new UserControls.NewPlaylistControl(AddNewPlaylist);
+            if (currentPlaylistType == PlaylistPageType.Music)
+            {
+                NewPlaylistControl newPlaylistControl = new NewPlaylistControl(AddNewMusicPlaylist);
+                MainWindow.ShowCustomMessageBoxInMiddle(newPlaylistControl);
+            }
+            else if (currentPlaylistType == PlaylistPageType.Video)
+            {
 
-            MainWindow.ShowCustomMessageBoxInMiddle(newPlaylistControl);
+            } 
+            else if (currentPlaylistType == PlaylistPageType.Image)
+            {
+
+            }
+
             e.Handled = true;
         }
 
-        private void AddNewPlaylist(string playlistName)
-        {
-            SongPlaylistModel playlist = new SongPlaylistModel(playlistName);
-            this.playlists.Add(playlist);
-
-            PlaylistDatabaseAccess.SavePlaylist(playlist);
-
-            MainWindow.ClearAllMessageBox();
-
-            if (playlists.Count > 0)
-            {
-                emptyPlaylistBorder.Visibility = Visibility.Collapsed;
-                newPlaylistBtn.Visibility = Visibility.Visible;
-            }
-        }
-
-        private void RemovePlaylist()
-        {
-            SongPlaylistModel? playlist;
-            playlist = playlistListBox.SelectedItem as SongPlaylistModel;
-
-            if (playlist != null)
-            {
-                this.playlists.Remove(playlist);
-                PlaylistDatabaseAccess.DeletePlaylist(playlist);
-            }
-
-            if (playlists.Count <= 0)
-            {
-                emptyPlaylistBorder.Visibility = Visibility.Visible;
-                newPlaylistBtn.Visibility = Visibility.Collapsed;
-            }
-        }
-
-        private void RenamePlaylist(string newName)
-        {
-            SongPlaylistModel? playlist;
-            playlist = playlistListBox.SelectedItem as SongPlaylistModel;
-
-            if (playlist != null)
-            {
-                SongPlaylistModel renamedPlaylist = new SongPlaylistModel(playlist);
-                PlaylistDatabaseAccess.RenamePlaylist(renamedPlaylist);
-                renamedPlaylist.Name = newName;
-
-                for (int i = 0; i < this.playlists.Count; i++)
-                {
-                    if (playlist.Id == this.playlists[i].Id)
-                    {
-                        playlists.RemoveAt(i);
-                        playlists.Insert(i, renamedPlaylist);
-                        break;
-                    }
-                }
-            }
-        }
-
-        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        private void backBtn_Click(object sender, RoutedEventArgs e)
         {
             playlistNameHeader.Text = "Playlist";
-            BackBtn.Visibility = Visibility.Collapsed;
-            playlistListBox.Visibility = Visibility.Visible;
             cbSortPlaylistBy.Visibility = Visibility.Visible;
-            playlistSongsDataGrid.Visibility = Visibility.Collapsed;
-            addNewSongToPlaylistBtn.Visibility = Visibility.Collapsed;
             newPlaylistBtn.Visibility = Visibility.Visible;
+            chooseCategoryPanel.Visibility = Visibility.Visible;
+            backBtn.Visibility = Visibility.Collapsed;
+
+            if (currentPlaylistType == PlaylistPageType.Music)
+            {
+                addNewSongToPlaylistBtn.Visibility = Visibility.Collapsed;
+
+                PlaylistPagePages.MusicPlaylistPage.PlaylistListBox.Visibility = Visibility.Visible;
+                PlaylistPagePages.MusicPlaylistPage.PlaylistSongsDataGrid.Visibility = Visibility.Collapsed;
+            }
+            else if (currentPlaylistType == PlaylistPageType.Video)
+            {
+
+            }
+            else if (currentPlaylistType == PlaylistPageType.Image)
+            {
+
+            }
 
             MainPage.currentMusicBrowsingFolder = "Playlist";
             MainPage.Instance.ChangeButtonsViewOnOpenFolder(forceShow: true);
             MainPage.Instance.ChangeAdditionControlVisibilityInInforGrid(false);
         }
 
+        private void AddNewMusicPlaylist(string playlistName)
+        {
+            SongPlaylistModel playlist = new SongPlaylistModel(playlistName);
+            PlaylistPagePages.MusicPlaylistPage.playlists.Add(playlist);
+
+            PlaylistDatabaseAccess.SavePlaylist(playlist);
+
+            MainWindow.ClearAllMessageBox();
+
+            if (PlaylistPagePages.MusicPlaylistPage.playlists.Count > 0)
+            {
+                PlaylistPagePages.MusicPlaylistPage.EmptyPlaylistBorder.Visibility = Visibility.Collapsed;
+                PlaylistPage.NewPlaylistBtn.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void cbSortPlaylistBy_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbSortPlaylistBy.SelectedItem != null)
+            {
+                int sortIndex = cbSortPlaylistBy.SelectedIndex;
+                if (sortIndex == 0)
+                {
+                    List<SongPlaylistModel> tempPlaylists = new List<SongPlaylistModel>(PlaylistPagePages.MusicPlaylistPage.playlists);
+                    tempPlaylists.Sort((x, y) => x.Name.CompareTo(y.Name));
+
+                    PlaylistPagePages.MusicPlaylistPage.playlists.Clear();
+                    foreach (SongPlaylistModel song in tempPlaylists) PlaylistPagePages.MusicPlaylistPage.playlists.Add(song);
+                } 
+                else if (sortIndex == 1)
+                {
+                    List<SongPlaylistModel> tempPlaylists = new List<SongPlaylistModel>(PlaylistPagePages.MusicPlaylistPage.playlists);
+                    tempPlaylists.Sort((x, y) => 
+                        DateTime.Parse(y.TimeCreated, CultureInfo.InvariantCulture)
+                        .CompareTo(DateTime.Parse(x.TimeCreated, CultureInfo.InvariantCulture)));
+
+                    PlaylistPagePages.MusicPlaylistPage.playlists.Clear();
+                    foreach (SongPlaylistModel song in tempPlaylists) PlaylistPagePages.MusicPlaylistPage.playlists.Add(song);
+                }
+            }
+        }
+
+
         private void addNewSongToPlaylistBtn_Click(object sender, RoutedEventArgs e)
         {
             SongPlaylistModel? playlist;
-            playlist = playlistListBox.SelectedItem as SongPlaylistModel;
+            playlist = PlaylistPagePages.MusicPlaylistPage.PlaylistListBox.SelectedItem as SongPlaylistModel;
 
             if (playlist != null)
             {
@@ -245,7 +224,7 @@ namespace GalaxyMediaPlayer.Pages.NavContentPages
                         if (PlaylistSongsDatabaseAccess.SaveSong(newSongInfor) == 1)
                         {
                             playlist.Songs.Add(newSongInfor);
-                            currentChosenPlaylistSongs.Add(newSongInfor);
+                            PlaylistPagePages.MusicPlaylistPage.currentChosenPlaylistSongs.Add(newSongInfor);
                         }
                         else errorMessages += newSongInfor.Name + " already exists in playlist\n";
 
@@ -257,88 +236,6 @@ namespace GalaxyMediaPlayer.Pages.NavContentPages
                     }
                 }
             }
-        }
-
-        private void showMusicPlaylistsBtn_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            showMusicPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.White;
-            showVideosPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.Transparent;
-            showImagesPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.Transparent;
-        }
-
-        private void showVideosPlaylistsBtn_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-
-            showMusicPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.Transparent;
-            showVideosPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.White;
-            showImagesPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.Transparent;
-        }
-
-        private void showImagesPlaylistsBtn_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-
-            showMusicPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.Transparent;
-            showVideosPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.Transparent;
-            showImagesPlaylistsBtn.BorderBrush = System.Windows.Media.Brushes.White;
-        }
-
-        private void cbSortPlaylistBy_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (cbSortPlaylistBy.SelectedItem != null)
-            {
-                int sortIndex = cbSortPlaylistBy.SelectedIndex;
-                if (sortIndex == 0)
-                {
-                    List<SongPlaylistModel> tempPlaylists = new List<SongPlaylistModel>(playlists);
-                    tempPlaylists.Sort((x, y) => x.Name.CompareTo(y.Name));
-
-                    playlists.Clear();
-                    foreach (SongPlaylistModel song in tempPlaylists) playlists.Add(song);
-                } 
-                else if (sortIndex == 1)
-                {
-                    List<SongPlaylistModel> tempPlaylists = new List<SongPlaylistModel>(playlists);
-                    tempPlaylists.Sort((x, y) => 
-                        DateTime.Parse(y.TimeCreated, CultureInfo.InvariantCulture)
-                        .CompareTo(DateTime.Parse(x.TimeCreated, CultureInfo.InvariantCulture)));
-
-                    playlists.Clear();
-                    foreach (SongPlaylistModel song in tempPlaylists) playlists.Add(song);
-                }
-            }
-        }
-
-        private void DeleteCheckBoxClick(object sender, RoutedEventArgs e)
-        {
-            if (sender == null) return;
-
-            CheckBox cb = (CheckBox)sender;
-            if ((bool)cb.IsChecked) deleteIndices.Add(playlistSongsDataGrid.SelectedIndex);
-            else deleteIndices.Remove(playlistSongsDataGrid.SelectedIndex);
-            e.Handled = true;
-        }
-
-        private void deleteIconHeader_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (deleteIndices.Count > 0)
-            {
-                ConfirmDialog dialog = new ConfirmDialog("Delete Songs", "Are you sure to delete " + deleteIndices.Count + " songs", deleteSongsInPlaylist);
-
-                dialog.Show();
-            }
-        }
-
-        private void deleteSongsInPlaylist()
-        {
-            List<SongInfor> deleteSongs = new List<SongInfor>();
-            foreach (int idx in deleteIndices.OrderByDescending(v => v))
-            {
-                deleteSongs.Add(currentChosenPlaylistSongs[idx]);
-                currentChosenPlaylistSongs.RemoveAt(idx);
-            }
-
-            deleteIndices.Clear();
-            PlaylistSongsDatabaseAccess.DeleteSongs(deleteSongs);
         }
     }
 }
---
+++ GalaxyMediaPlayer/Pages/PlaylistPagePages/MusicPlaylistPage.xaml.cs
@@ -0,0 +1,202 @@
+﻿using GalaxyMediaPlayer.Databases.SongPlaylist;
+using GalaxyMediaPlayer.Helpers;
+using GalaxyMediaPlayer.Models;
+using GalaxyMediaPlayer.UserControls;
+using GalaxyMediaPlayer.UserControls.PlaylistControls;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using GalaxyMediaPlayer.Pages.NavContentPages;
+
+namespace GalaxyMediaPlayer.Pages.PlaylistPagePages
+{
+    /// <summary>
+    /// Interaction logic for MusicPlaylistPage.xaml
+    /// </summary>
+    public partial class MusicPlaylistPage : Page
+    {
+        public static ObservableCollection<SongPlaylistModel> playlists;
+        public static ObservableCollection<SongInfor> currentChosenPlaylistSongs = new ObservableCollection<SongInfor>();
+        private List<int> deleteIndices = new List<int>();
+
+        public static ListBox PlaylistListBox;
+        public static DataGrid PlaylistSongsDataGrid;
+        public static Border EmptyPlaylistBorder;
+
+        public MusicPlaylistPage()
+        {
+            InitializeComponent();
+
+            playlists = new ObservableCollection<SongPlaylistModel>(PlaylistDatabaseAccess.LoadPlaylists());
+
+            PlaylistListBox = playlistListBox;
+            PlaylistSongsDataGrid = playlistSongsDataGrid;
+            EmptyPlaylistBorder = emptyPlaylistBorder;
+
+            playlistListBox.ItemsSource = playlists;
+            playlistSongsDataGrid.ItemsSource = currentChosenPlaylistSongs;
+
+            if (playlists.Count <= 0)
+            {
+                emptyPlaylistBorder.Visibility = Visibility.Visible;
+                PlaylistPage.NewPlaylistBtn.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void listBoxItem_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            SongPlaylistModel? playlist;
+            playlist = playlistListBox.SelectedItem as SongPlaylistModel;
+
+            if (playlist != null && e.ClickCount == 2)
+            {
+                MainPage.currentMusicBrowsingFolder = "Playlist/" + playlist.Name;
+
+                currentChosenPlaylistSongs.Clear();
+                foreach (SongInfor songInfor in PlaylistSongsDatabaseAccess.LoadSongsFromPlaylistId(playlist.Id))
+                {
+                    if (File.Exists(songInfor.Path))
+                        currentChosenPlaylistSongs.Add(songInfor);
+                    else PlaylistSongsDatabaseAccess.DeleteSong(songInfor);
+                }
+
+                MainPage.Instance.ChangeButtonsViewOnOpenFolder(forceShow: false);
+                MainPage.Instance.ChangeAdditionControlVisibilityInInforGrid(false);
+
+                playlistSongsDataGrid.Visibility = Visibility.Visible;
+                playlistListBox.Visibility = Visibility.Collapsed;
+
+                PlaylistPage.ChooseCategoryPanel.Visibility = Visibility.Collapsed;
+                PlaylistPage.PlaylistNameHeader.Text = playlist.Name;
+                PlaylistPage.CbSortPlaylistBy.Visibility = Visibility.Collapsed;
+                PlaylistPage.BackBtn.Visibility = Visibility.Visible;
+                PlaylistPage.AddNewSongToPlaylistBtn.Visibility = Visibility.Visible;
+                PlaylistPage.NewPlaylistBtn.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void listBoxItem_MouseRightButtonDown(
+            object sender,
+            MouseButtonEventArgs e)
+        {
+            SongPlaylistModel? playlist;
+            playlist = playlistListBox.SelectedItem as SongPlaylistModel;
+
+            if (playlist != null)
+            {
+                PlaylistRightClickDialog dialog = new PlaylistRightClickDialog(
+                    onRenameButtonClick: RenamePlaylist,
+                    onDeleteButtonClick: RemovePlaylist,
+                    currentName: playlist.Name);
+
+                int left = Convert.ToInt32(e.GetPosition(MainWindow.Instance as IInputElement).X);
+                int top = Convert.ToInt32(e.GetPosition(MainWindow.Instance as IInputElement).Y);
+                MainWindow.ShowCustomMessageBox(dialog, left: left, top: top);
+                e.Handled = true;
+            }
+        }
+
+        private void RemovePlaylist()
+        {
+            SongPlaylistModel? playlist;
+            playlist = playlistListBox.SelectedItem as SongPlaylistModel;
+
+            if (playlist != null)
+            {
+                playlists.Remove(playlist);
+                PlaylistDatabaseAccess.DeletePlaylist(playlist);
+            }
+
+            if (playlists.Count <= 0)
+            {
+                emptyPlaylistBorder.Visibility = Visibility.Visible;
+                PlaylistPage.NewPlaylistBtn.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void RenamePlaylist(string newName)
+        {
+            SongPlaylistModel? playlist;
+            playlist = playlistListBox.SelectedItem as SongPlaylistModel;
+
+            if (playlist != null)
+            {
+                SongPlaylistModel renamedPlaylist = new SongPlaylistModel(playlist);
+                PlaylistDatabaseAccess.RenamePlaylist(renamedPlaylist);
+                renamedPlaylist.Name = newName;
+
+                for (int i = 0; i < playlists.Count; i++)
+                {
+                    if (playlist.Id == playlists[i].Id)
+                    {
+                        playlists.RemoveAt(i);
+                        playlists.Insert(i, renamedPlaylist);
+                        break;
+                    }
+                }
+            }
+        }
+
+        // DataGridRow hold songs that is currently in the chosen playlist
+        private void DataGridRow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            SongInfor? chosenSong;
+            chosenSong = playlistSongsDataGrid.SelectedItem as SongInfor;
+
+            // Nam: indicates that a song is chosen (not outside)
+            if (chosenSong != null)
+            {
+                List<string> songs = playlistSongsDataGrid.Items.Cast<SongInfor>().Select(s => s.Path).ToList();
+                MyMediaPlayer.SetNewPlaylist(songs);
+                MyMediaPlayer.SetPositionInPlaylist(playlistSongsDataGrid.SelectedIndex);
+                MyMediaPlayer.PlayCurrentSong();
+                e.Handled = true;
+            }
+        }
+
+        private void DeleteCheckBoxClick(object sender, RoutedEventArgs e)
+        {
+            if (sender == null) return;
+
+            CheckBox cb = (CheckBox)sender;
+            if ((bool)cb.IsChecked) deleteIndices.Add(playlistSongsDataGrid.SelectedIndex);
+            else deleteIndices.Remove(playlistSongsDataGrid.SelectedIndex);
+            e.Handled = true;
+        }
+
+        private void deleteIconHeader_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (deleteIndices.Count > 0)
+            {
+                ConfirmDialog dialog = new ConfirmDialog("Delete Songs", "Are you sure to delete " + deleteIndices.Count + " songs", deleteSongsInPlaylist);
+
+                dialog.Show();
+            }
+        }
+
+        private void deleteSongsInPlaylist()
+        {
+            List<SongInfor> deleteSongs = new List<SongInfor>();
+            foreach (int idx in deleteIndices.OrderByDescending(v => v))
+            {
+                deleteSongs.Add(currentChosenPlaylistSongs[idx]);
+                currentChosenPlaylistSongs.RemoveAt(idx);
+            }
+
+            deleteIndices.Clear();
+            PlaylistSongsDatabaseAccess.DeleteSongs(deleteSongs);
+        }
+
+        private void secondaryNewPlaylistBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PlaylistPage.NewPlaylistBtn_Click(sender, e);
+        }
+    }
+}
