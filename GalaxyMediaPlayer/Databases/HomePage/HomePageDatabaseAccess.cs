1a4ed912af10d891ef0c1ae145102f02b7b3236e
Nam Huỳnh <21522356@ms.uit.edu.vn>
2023-01-25 20:56:01 +0700

    done most listened feature

---
+++ GalaxyMediaPlayer/Databases/HomePage/HomePageDatabaseAccess.cs
@@ -0,0 +1,80 @@
+﻿using GalaxyMediaPlayer.Models;
+using System.Collections.Generic;
+using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace GalaxyMediaPlayer.Databases.HomePage
+{
+    internal class HomePageDatabaseAccess
+    {
+        public static List<Pages.HomePage.MainPage.MostWatchEntity> GetMostWatchedEntities()
+        {
+            using (IDbConnection connection = new SQLiteConnection(GetConnectionStr()))
+            {
+                var output = connection.Query<Pages.HomePage.MainPage.MostWatchEntity>("select * from MostWatched");
+                return output.ToList();
+            }
+        }
+
+        public static List<Pages.HomePage.MainPage.MostWatchEntity> GetMostListenedEntities()
+        {
+            using (IDbConnection connection = new SQLiteConnection(GetConnectionStr()))
+            {
+                var output = connection.Query<Pages.HomePage.MainPage.MostWatchEntity>("select * from MostListened");
+                return output.ToList();
+            }
+        }
+
+        // Nam: return number is the number of rows affected when saving
+        public static int SaveDataOnWatchingVideo(string videoPath)
+        {
+            using (IDbConnection connection = new SQLiteConnection(GetConnectionStr()))
+            {
+                List<Pages.HomePage.MainPage.MostWatchEntity> queryResult = 
+                    connection.Query<Pages.HomePage.MainPage.MostWatchEntity>("select * from MostWatched where (Path)=@Path", new { Path = videoPath }).ToList();
+
+                if (queryResult.Count == 1)
+                {
+                    queryResult[0].Count++;
+                    int rowAffected = connection.Execute("update MostWatched set Count=@Count where Path=@Path", queryResult[0]);
+                    return rowAffected;
+                } 
+                else
+                {
+                    int rowAffected = connection.Execute("insert into MostWatched (Path, Count) values (@Path, @Count)", new Pages.HomePage.MainPage.MostWatchEntity(videoPath, 1));
+                    return rowAffected;
+                }
+            }
+        }
+
+        // Nam: return number is the number of rows affected when saving
+        public static int SaveDataOnListeningMusic(string musicPath)
+        {
+            using (IDbConnection connection = new SQLiteConnection(GetConnectionStr()))
+            {
+                List<Pages.HomePage.MainPage.MostWatchEntity> queryResult =
+                    connection.Query<Pages.HomePage.MainPage.MostWatchEntity>("select * from MostListened where (Path)=@Path", new { Path = musicPath }).ToList();
+
+                if (queryResult.Count == 1)
+                {
+                    queryResult[0].Count++;
+                    int rowAffected = connection.Execute("update MostListened set Count=@Count where Path=@Path", queryResult[0]);
+                    return rowAffected;
+                }
+                else
+                {
+                    int rowAffected = connection.Execute("insert into MostListened (Path, Count) values (@Path, @Count)", new Pages.HomePage.MainPage.MostWatchEntity(musicPath, 1));
+                    return rowAffected;
+                }
+            }
+        }
+
+        private static string GetConnectionStr()
+        {
+            return ConfigurationManager.ConnectionStrings["HomePageDbConnStr"].ConnectionString;
+        }
+    }
+}
--- GalaxyMediaPlayer/Pages/NavContentPages/HomePage/MainPage.xaml.cs
+++ GalaxyMediaPlayer/Pages/NavContentPages/HomePage/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GalaxyMediaPlayer.Databases.HomePage;
+using System;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Windows.Controls;
@@ -10,14 +11,17 @@ namespace GalaxyMediaPlayer.Pages.HomePage
     /// </summary>
     public partial class MainPage : Page
     {
-        private class MostWatchEntity
+        public class MostWatchEntity
         {
             public string Path { get; set; }
             public int Count { get; set; }
             public string Title
             {
                 get { return System.IO.Path.GetFileNameWithoutExtension(Path); }
+                set { Title = value; }
             }
+
+            public MostWatchEntity() { }
 
             public MostWatchEntity(string path, int count)
             {
@@ -27,6 +31,7 @@ namespace GalaxyMediaPlayer.Pages.HomePage
         }
 
         private ObservableCollection<MostWatchEntity> mostWatchedEntities = new ObservableCollection<MostWatchEntity>();
+        private ObservableCollection<MostWatchEntity> mostListenedEntities = new ObservableCollection<MostWatchEntity>();
 
         public MainPage()
         {
@@ -36,15 +41,18 @@ namespace GalaxyMediaPlayer.Pages.HomePage
             else if (DateTime.Now.ToString("tt") == "PM") greetingTb.Text = "Good Morning,";
 
             mostWatchListbox.ItemsSource = mostWatchedEntities;
-            mostWatchedEntities.Add(new MostWatchEntity(@"C:\Users\hthna\Downloads\Die For You - VALORANT Champions 2021.mp3", 0));
-            mostWatchedEntities.Add(new MostWatchEntity(@"C:\Users\hthna\Desktop\24kGoldn - Mood.mp4", 0));
+            mostListenedListbox.ItemsSource = mostListenedEntities;
 
-            
+            foreach (var item in HomePageDatabaseAccess.GetMostWatchedEntities()) mostWatchedEntities.Add(item);
+            foreach (var item in HomePageDatabaseAccess.GetMostListenedEntities()) mostListenedEntities.Add(item);
+
+            if (mostWatchedEntities.Count == 0) mostWatchedTb.Visibility = System.Windows.Visibility.Collapsed;
+            if (mostListenedEntities.Count == 0) mostListenedTb.Visibility = System.Windows.Visibility.Collapsed;
         }
 
         private void listboxItem_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-
+            
         }
     }
 }
