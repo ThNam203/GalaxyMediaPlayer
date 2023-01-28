87b2bf848217516857850d43a2fa1eed666db4e6
ptdat4823 <phamtiendat4823@gmail.com>
2023-01-28 11:17:48 +0700

    add basic ImagePlaylistPage

---
+++ GalaxyMediaPlayer/Databases/ImagePage/ImagesPlaylistDBAccess.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using GalaxyMediaPlayer.Models;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+
+
+namespace GalaxyMediaPlayer.Databases.ImagePage
+{
+    public class ImagesPlaylistDBAccess
+    {
+        public static List<ImagePlaylistModel> LoadImagePlayList()
+        {
+            using (IDbConnection connStr = new SQLiteConnection(GetConnectionStr()))
+            {
+                var output = connStr.Query<ImagePlaylistModel>("select * from ImagePlaylistTable");
+                return output.ToList();
+            }
+        }
+
+        public static int SaveImagePlaylist(ImagePlaylistModel newImagePlaylist)
+        {
+            using (IDbConnection connStr = new SQLiteConnection(GetConnectionStr()))
+            {
+                bool isExisted = connStr.ExecuteScalar<bool>("select count(1) from ImagePlaylistTable where Id=@Id", newImagePlaylist);
+
+                if (!isExisted)
+                {
+                    int rowAffected = connStr.Execute("insert into ImagePlaylistTable (Id, PlaylistName) values (@Id, @PlaylistName)", newImagePlaylist);
+                    return rowAffected;
+                }
+                else
+                    return -1;
+            }
+        }
+
+        public static int DeleteImagePlaylist(ImagePlaylistModel newImagePlaylist)
+        {
+            using (IDbConnection connStr = new SQLiteConnection(GetConnectionStr()))
+            {
+                int rowAffected = connStr.Execute("Delete from ImagePlaylistTable where Id=@Id", newImagePlaylist);
+                return rowAffected;
+            }
+        }
+
+        private static string GetConnectionStr()
+        {
+            return ConfigurationManager.ConnectionStrings["ImagesDBConnStr"].ConnectionString;
+        }
+    }
+}
---
+++ GalaxyMediaPlayer/Pages/PlaylistPagePages/ImagePlaylistPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using GalaxyMediaPlayer.Databases.ImagePage;
+using GalaxyMediaPlayer.Models;
+using GalaxyMediaPlayer.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GalaxyMediaPlayer.Pages.PlaylistPagePages
+{
+    /// <summary>
+    /// Interaction logic for ImagePlaylistPage.xaml
+    /// </summary>
+    public partial class ImagePlaylistPage : Page
+    {
+        private static ObservableCollection<ImagePlaylistModel> ImagesPlaylist;
+        public ImagePlaylistPage()
+        {
+            InitializeComponent();
+            ImagesPlaylist = new ObservableCollection<ImagePlaylistModel>(ImagesPlaylistDBAccess.LoadImagePlayList());
+
+            foreach(ImagePlaylistModel imagePlaylistModel in ImagesPlaylist)
+            {
+                ListBoxImagePlaylist.Items.Add(imagePlaylistModel);
+            }
+
+            if(ImagesPlaylist.Count > 0)
+            {
+                ShowButtonWhenHaveImagePlaylist();
+            }
+        }
+
+        void ShowButtonWhenDoNotHaveImagePlaylist()
+        {
+            BorderlistView.Visibility = Visibility.Visible;
+            ListBoxImagePlaylist.Visibility = Visibility.Collapsed;
+        }
+
+        void ShowButtonWhenHaveImagePlaylist()
+        {
+            BorderlistView.Visibility = Visibility.Collapsed;
+            ListBoxImagePlaylist.Visibility = Visibility.Visible;
+        }
+
+        private void Btn_AddNow_Click(object sender, RoutedEventArgs e)
+        {
+            NewPlaylistControl newPlaylistControl = new NewPlaylistControl(AddNewImagePlaylist);
+            MainWindow.ShowCustomMessageBoxInMiddle(newPlaylistControl);
+        }
+
+        private void AddNewImagePlaylist(string PlaylistName)
+        {
+            ImagePlaylistModel imagePlaylistModel = new ImagePlaylistModel(PlaylistName);
+            ImagesPlaylist.Add(imagePlaylistModel);
+            ListBoxImagePlaylist.Items.Add(imagePlaylistModel);
+
+            ImagesPlaylistDBAccess.SaveImagePlaylist(imagePlaylistModel);
+            MainWindow.ClearAllMessageBox();
+            if (ImagesPlaylist.Count > 0)
+            {
+                ShowButtonWhenHaveImagePlaylist();
+            }
+        }
+
+        private void listBoxItem_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void listBoxItem_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
